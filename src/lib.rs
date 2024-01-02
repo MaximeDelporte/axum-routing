8a4902d9470a6828cf4c847ea837ef2f0e5342ae
Maxime Delporte <maxime@delporte.dev>
2024-01-02 14:17:18 +0100

    Create create_routes function inside mod.rs. Handle / endpoint inside hello_world.rs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-use::tokio;
-use axum::{routing::{get}, Router};
+mod routes;
+use crate::routes::create_routes;
 
 pub async fn run() {
-    let app = Router::new().route("/", get(|| async { "Hello World" }));
+    let app = create_routes();
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
