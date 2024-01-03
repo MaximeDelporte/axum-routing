fa3c2b6da600ec67f0c8355e70650317b3dc3fe0
Maxime Delporte <maxime@delporte.dev>
2024-01-03 18:52:04 +0100

    Update headers.rs adding User-Agent property inside the response. Update the way the header is parsed.

--- src/routes/headers.rs
+++ src/routes/headers.rs
@@ -1,11 +1,12 @@
-use axum::http::header::{CONTENT_LENGTH};
+use axum::http::header::{CONTENT_LENGTH, USER_AGENT};
 use axum::http::HeaderMap;
 
 pub async fn headers(headers: HeaderMap) -> String {
     let content_length_result = headers.get(CONTENT_LENGTH).unwrap().to_str();
+    let content_length = content_length_result.ok().unwrap();
 
-    return match content_length_result {
-        Ok(content_length) => String::from(content_length),
-        _ => "An error happened".to_string(),
-    }
+    let user_agent_result = headers.get(USER_AGENT).unwrap().to_str();
+    let user_agent = user_agent_result.ok().unwrap();
+
+    return format!("Content-Length: {content_length}\nUser-Agent: {user_agent}");
 }
